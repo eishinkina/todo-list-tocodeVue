fb8d47575a0827d1732322784a9f6b63cd37c918
Ekaterina Ishinkina <esiforlearn@gmail.com>
2024-02-01 11:58:06 +0400

    fix commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,42 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <div class="message" v-if="message">
-      <p>{{ message }}</p>
-    </div>
-    <!-- //new-note -->
-    <div class="new-note">
-      <input v-model="note.title" type="text" placeholder="введите заметку" />
-      <textarea v-model="note.descr" placeholder="введите описание"></textarea>
-      <button @click="addNote">Новая заметка</button>
-    </div>
+  <div class="wrapper">
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+          <div>
+            <h1>{{ title }}</h1>
+            <div class="message" v-if="message">
+              <p>{{ message }}</p>
+            </div>
+            <!-- //new-note -->
+            <div class="new-note">
+              <input
+                v-model="note.title"
+                type="text"
+                placeholder="введите заметку"
+              />
+              <textarea
+                v-model="note.descr"
+                placeholder="введите описание"
+              ></textarea>
+              <button @click="addNote">Новая заметка</button>
+            </div>
 
-    <!-- //note-list -->
-    <div class="notes">
-      <div class="note" v-for="(note, index) in notes" :key="index">
-        <div class="note-header">
-          <p>{{ note.title }}</p>
+            <!-- //note-list -->
+            <div class="notes">
+              <div class="note" v-for="(note, index) in notes" :key="index">
+                <div class="note-header">
+                  <p>{{ note.title }}</p>
+                </div>
+                <div class="note-body">
+                  <p>{{ note.descr }}</p>
+                  <span>{{ formatRussianDate(note.date) }}</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="note-body">
-          <p>{{ note.descr }}</p>
-          <span>{{ formatRussianDate(note.date) }}</span>
-        </div>
-      </div>
+      </section>
     </div>
   </div>
 </template>
